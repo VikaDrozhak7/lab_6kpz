584cf0af08d55e4ad6d66595ed0b3960501a6264
VikaDrozhak <vt222_dvt@student.ztu.edu.ua>
2024-05-11 14:28:21 +0300

    added a new account class for performing operations on user accounts

---
+++ LibraryBank/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBank
+{
+    public partial class Account
+    {
+        public string CardNumber { get; set; }
+        public string OwnerName { get; set; }
+        public decimal Balance { get; set; }
+        private string PinCode { get; set; }
+
+        public string GetPinCode()
+        {
+            return PinCode;
+        }
+
+        public bool Authenticate(string enteredCardNumber, string enteredPin)
+        {
+            return this.CardNumber == enteredCardNumber && this.PinCode == enteredPin;
+        }
+    }
+}
---
+++ LibraryBank/AccountOperations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBank
+{
+    public partial class Account
+    {
+        public void WithdrawCash(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                OnCashWithdrawn();
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds in the account.");
+            }
+        }
+
+        public void DepositCash(decimal amount)
+        {
+            Balance += amount;
+            OnCashDeposited();
+        }
+
+        public void TransferFunds(Account toAccount, decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                toAccount.DepositCash(amount);
+                OnFundsTransferred();
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds in the account.");
+            }
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Account balance: {Balance}");
+            OnBalanceChecked();
+        }
+    }
+}
