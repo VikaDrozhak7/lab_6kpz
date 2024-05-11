b0be22038c09ff9ccda29101c10bd0ce83d49db7
VikaDrozhak <vt222_dvt@student.ztu.edu.ua>
2024-05-11 14:46:23 +0300

    added a new bank class and events for it

---
+++ LibraryBank/Bank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBank
+{
+    public partial class Bank
+    {
+        public string Name { get; set; }
+        public List<AutomatedTellerMachine> ATMs { get; set; }
+
+        public Bank(string name)
+        {
+            Name = name;
+            ATMs = new List<AutomatedTellerMachine>();
+        }
+
+        public void AddATM(AutomatedTellerMachine atm)
+        {
+            ATMs.Add(atm);
+        }
+
+        public AutomatedTellerMachine CreateATM()
+        {
+
+            AutomatedTellerMachine atm = new AutomatedTellerMachine();
+            AddATM(atm);
+            return atm;
+        }
+    }
+}
---
+++ LibraryBank/BankEvents.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBank
+{
+    public partial class Bank
+    {
+        public event EventHandler Authenticated;
+        public event EventHandler BalanceChecked;
+        public event EventHandler CashWithdrawn;
+        public event EventHandler CashDeposited;
+        public event EventHandler FundsTransferred;
+
+        protected virtual void OnAuthenticated()
+        {
+            Authenticated?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnBalanceChecked()
+        {
+            BalanceChecked?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnCashWithdrawn()
+        {
+            CashWithdrawn?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnCashDeposited()
+        {
+            CashDeposited?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnFundsTransferred()
+        {
+            FundsTransferred?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
